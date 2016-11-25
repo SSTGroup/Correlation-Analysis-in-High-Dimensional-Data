f5f10d1ea0f1d6312d5522fea009860816b0a1c9
tanujhasija <tanuj.hasija@sst.upb.de>
2016-11-25 17:57:16 +0100

    Update Det_13_Song.m

--- src/Det_13_Song.m
+++ src/Det_13_Song.m
@@ -1,7 +1,50 @@
+## ----------------------------------------------------------------------------
+##
+##   File: Det_13_Song.m
+##   Copyright (c) <2016> <University of Paderborn>
+##   Permission is hereby granted, free of charge, to any person
+##   obtaining a copy of this software and associated documentation
+##   files (the "Software"), to deal in the Software without restriction,
+##   including without limitation the rights to use, copy, modify and
+##   merge the Software, subject to the following conditions:
+##
+##   1.) The Software is used for non-commercial research and
+##       education purposes.
+##
+##   2.) The above copyright notice and this permission notice shall be
+##       included in all copies or substantial portions of the Software.
+##
+##   3.) Publication, Distribution, Sublicensing, and/or Selling of
+##       copies or parts of the Software requires special agreements
+##       with the University of Paderborn and is in general not permitted.
+##
+##   4.) Modifications or contributions to the software must be
+##       published under this license. The University of Paderborn
+##       is granted the non-exclusive right to publish modifications
+##       or contributions in future versions of the Software free of charge.
+##
+##   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+##   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+##   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+##   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+##   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+##   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+##   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+##   OTHER DEALINGS IN THE SOFTWARE.
+##
+##   Persons using the Software are encouraged to notify the
+##   Department of Communications Engineering at the University of Paderborn
+##   about bugs. Please reference the Software in your publications
+##   if it was used for them.
+##
+##
+##   Author: Tanuj Hasija
+##
+## ----------------------------------------------------------------------------
 function dcap = Det_13_Song(X1,X2,X3,X4,X5,M,P_fa)
 % MATLAB implementation based on ?Determining the number of signals correlated 
 % across multiple data sets for small sample support", 
-% Y. Song, T. Hasija, P. J. Schreier, and D. Ram?rez, EUSIPCO 2016 
+% Y. Song, T. Hasija, P. J. Schreier, and D. Ramirez, EUSIPCO 2016 
 
 
 % Max-Min for two data sets to estimate r1,r2,r3,r4,r5
