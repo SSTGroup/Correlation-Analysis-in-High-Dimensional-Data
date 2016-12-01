ce5c358622e13d179ed17a48323b253dcb6c14ba
tanujhasija <tanuj.hasija@sst.upb.de>
2016-12-01 13:34:05 +0100

    Update Max_Min_Five_Datasets.m

--- src/Max_Min_Five_Datasets.m
+++ src/Max_Min_Five_Datasets.m
@@ -1,46 +1,46 @@
-## ----------------------------------------------------------------------------
-##
-##   File: Max_Min_Five_Datasets.m
-##   Copyright (c) <2016> <University of Paderborn>
-##   Permission is hereby granted, free of charge, to any person
-##   obtaining a copy of this software and associated documentation
-##   files (the "Software"), to deal in the Software without restriction,
-##   including without limitation the rights to use, copy, modify and
-##   merge the Software, subject to the following conditions:
-##
-##   1.) The Software is used for non-commercial research and
-##       education purposes.
-##
-##   2.) The above copyright notice and this permission notice shall be
-##       included in all copies or substantial portions of the Software.
-##
-##   3.) Publication, Distribution, Sublicensing, and/or Selling of
-##       copies or parts of the Software requires special agreements
-##       with the University of Paderborn and is in general not permitted.
-##
-##   4.) Modifications or contributions to the software must be
-##       published under this license. The University of Paderborn
-##       is granted the non-exclusive right to publish modifications
-##       or contributions in future versions of the Software free of charge.
-##
-##   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-##   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-##   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-##   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-##   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-##   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-##   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-##   OTHER DEALINGS IN THE SOFTWARE.
-##
-##   Persons using the Software are encouraged to notify the
-##   Department of Communications Engineering at the University of Paderborn
-##   about bugs. Please reference the Software in your publications
-##   if it was used for them.
-##
-##
-##   Author: Tanuj Hasija
-##
-## ----------------------------------------------------------------------------
+% ## ----------------------------------------------------------------------------
+% ##
+% ##   File: Max_Min_Five_Datasets.m
+% ##   Copyright (c) <2016> <University of Paderborn>
+% ##   Permission is hereby granted, free of charge, to any person
+% ##   obtaining a copy of this software and associated documentation
+% ##   files (the "Software"), to deal in the Software without restriction,
+% ##   including without limitation the rights to use, copy, modify and
+% ##   merge the Software, subject to the following conditions:
+% ##
+% ##   1.) The Software is used for non-commercial research and
+% ##       education purposes.
+% ##
+% ##   2.) The above copyright notice and this permission notice shall be
+% ##       included in all copies or substantial portions of the Software.
+% ##
+% ##   3.) Publication, Distribution, Sublicensing, and/or Selling of
+% ##       copies or parts of the Software requires special agreements
+% ##       with the University of Paderborn and is in general not permitted.
+% ##
+% ##   4.) Modifications or contributions to the software must be
+% ##       published under this license. The University of Paderborn
+% ##       is granted the non-exclusive right to publish modifications
+% ##       or contributions in future versions of the Software free of charge.
+% ##
+% ##   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+% ##   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+% ##   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+% ##   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+% ##   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+% ##   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+% ##   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+% ##   OTHER DEALINGS IN THE SOFTWARE.
+% ##
+% ##   Persons using the Software are encouraged to notify the
+% ##   Signal and System Theory Group at the University of Paderborn
+% ##   about bugs. Please reference the Software in your publications
+% ##   if it was used for them.
+% ##
+% ##
+% ##   Author: Tanuj Hasija
+% ##
+% ## ----------------------------------------------------------------------------
 
 function [d_cap, r_d] = Max_Min_Five_Datasets(M, P_fa, Vx1,Vx2x3x4x5,Vx2,Vx3x4x5...
     ,Vx3, Vx4x5, Vx4, Vx5, rmax)
@@ -52,7 +52,7 @@ function [d_cap, r_d] = Max_Min_Five_Datasets(M, P_fa, Vx1,Vx2x3x4x5,Vx2,Vx3x4x5
 
 d_cap_vector = zeros(1,rmax);
 
-parfor r=1: rmax
+for r=1: rmax
     
     Kx1_x2x3x4x5 = svd(Vx1(:,1:r)'*Vx2x3x4x5(:,1:4*r));
     Kx2_x3x4x5 = svd(Vx2(:,1:r)'*Vx3x4x5(:,1:3*r));
